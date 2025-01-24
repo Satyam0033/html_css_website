d2f4439707dd537e51c514d8a3aed9aed4b142ca
Satyam0033 <satyamthakur0503@gmail.com>
2025-01-24 21:05:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-var typed = new Typed(".multiple-text" , {
-    Strings:["FRONTEND DEVELOPER", "HTML", "CSS", "JS"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-})
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+            });
+        };
+    });
+    /*======== sticky navbar ========*/
+let header = document.querySelector('header');
+
+header.classList.toggle('sticky', window.scrollY > 100);
+
+};
